75dda5ee2eecfe66531a3db8c4afcae7265b1aba
HoeSeong123 <ghltjd3@gmail.com>
2023-12-14 16:50:14 +0900

    refactor : 입력 시 양쪽 공백 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class InputView {
     public static int readVisitDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
+        String input = Console.readLine().trim();
         VisitDateValidator.validate(input);
 
         return Integer.parseInt(input);
@@ -17,7 +17,7 @@ public class InputView {
 
     public static Map<String, Integer> readOrderMenus() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String input = Console.readLine();
+        String input = Console.readLine().trim();
         OrderMenusValidator.validate(input);
 
         return Formatter.formatOrderMenus(input);
