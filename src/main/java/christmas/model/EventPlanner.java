cf476528da00b9a38284f9ab76b11ab59c19898c
HoeSeong123 <ghltjd3@gmail.com>
2023-12-14 16:33:33 +0900

    feat : 특별 할인 기능 구현

--- src/main/java/christmas/model/EventPlanner.java
+++ src/main/java/christmas/model/EventPlanner.java
@@ -1,17 +1,20 @@
 package christmas.model;
 
 import static christmas.model.Event.CHRISTMAS_D_DAY_DISCOUNT;
+import static christmas.model.Event.SPECIAL_DISCOUNT;
 import static christmas.model.Event.WEEKDAY_DISCOUNT;
 import static christmas.model.Event.WEEKEND_DISCOUNT;
 
 import christmas.repository.EventRepository;
 import java.time.LocalDate;
+import java.util.List;
 
 public class EventPlanner {
     public static void findEvents(int visitDate, OrderMenus orderMenus) {
         checkChristmasEvent(visitDate);
         checkWeekdayEvent(visitDate, orderMenus);
         checkWeekendEvent(visitDate, orderMenus);
+        checkSpecialEvent(visitDate);
     }
 
     private static void checkChristmasEvent(int visitDate) {
@@ -29,6 +32,13 @@ public class EventPlanner {
     private static void checkWeekendEvent(int visitDate, OrderMenus orderMenus) {
         if (CalenderChecker.isWeekend(LocalDate.of(2023, 12, visitDate))) {
             EventRepository.addEvent(WEEKEND_DISCOUNT, orderMenus.calculateWeekendEvent());
+        }
+    }
+
+    private static void checkSpecialEvent(int visitDate) {
+        List<Integer> specialDate = List.of(3, 10, 17, 24, 25, 31);
+        if (specialDate.contains(visitDate)) {
+            EventRepository.addEvent(SPECIAL_DISCOUNT, 1000);
         }
     }
 }
